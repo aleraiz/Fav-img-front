5620e67be5ee29085edc52d3119eac07f485354f
aleraiz <alejandroraiz7@gmail.com>
2023-05-08 15:04:51 -0300

    cambios en notas

--- src/pages/notas.jsx
+++ src/pages/notas.jsx
@@ -28,9 +28,21 @@
 // NextJs siempre se va a fijar primero si dentro de la carpeta productos hay una page con el nombre del [productId] si hay, renderiza la page, sino la ruta dinamica
 // Si dentro de la carpeta productos,tenemos una page x ejemplo camisa.jsx y una ruta dinamica es localhost:3000/productos/camisa se va a renderizar la page que tenemos en la carpeta
 
-// navegación entre rutas de la aplicacion:
+///////////////////            Catch all routes             //////////////////////////
+// https://www.youtube.com/watch?v=ZHn726VDoIY&list=PLC3y8-rFHvwgC9mj0qv972IO5DmD-H0ZH&index=9
+
+////////////////////              Link Component Navegation              //////////////////////
+
+// navegación entre rutas de la aplicacion utilizando Link Component:
 //Usamos el componente <Link href="/contacto">contacto</Link> en href va hacia donde queremos navegar (importar link component => import Link from 'next/link')
 // Ya no se necesita agregar un <a></a> dentro del link (en la mayoria de los tutoriales esta de esa manera pero se cambio la forma)
+// Si agregamos la propiedad replace (<Link href="/productos" replace>) al navegar hacia atras te lleva directamente a la pagina ="/"
+
+////////////////////              Navigating Programmatically              //////////////////////
+//Navegar automaticamente despues de que hacer algo, por ejemplo si enviamos un formulario y esta ok que nos lleve a otra pagina
+
+////////////////////              Custom 404 Page             //////////////////////
+// Ya existe una pagina 404 por defecto, pero si queremos crear una y editarla hay que crear un archivo 404.jsx o js.
 
 // ESTILOS CSS
 // Si queremos darle estilos a elementos generales para toda la aplicación, (por ejemplo darle estilos a una etiqueta h1) debemos hacerlo en global.css
