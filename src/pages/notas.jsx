befc01e88a3dfe9b857f8bcb84c33339fd2b8911
aleraiz <alejandroraiz7@gmail.com>
2023-05-09 17:59:13 -0300

    n

--- src/pages/notas.jsx
+++ src/pages/notas.jsx
@@ -44,6 +44,53 @@
 ////////////////////              Custom 404 Page             //////////////////////
 // Ya existe una pagina 404 por defecto, pero si queremos crear una y editarla hay que crear un archivo 404.jsx o js.
 
+////////////////////              Pre-rendering             //////////////////////
+// Se refiere al proceso de generar html por adelantado con los datos necesarios para una página en nuestra aplicación
+
+// Por que querriamos renderizar paginas previamente en una aplicación?
+// 1) Pre-rendering mejora el rendimiento.
+// En una app de react, tenemos que esperar a que se ejecute el javascript, que luego quizas se obtengan datos de una API externa y finalmente se renderiza el ui, por lo que hay un tiempo de espera del usuario.
+// Con una pagina pre-renderizada previamente, el html ya se generó y por lo tanto se carga más rápido. Por lo que el pre-rendering puede generar un mejor rendimiento
+// 2) Pre-rendering mejora el SEO.
+// En react, si el motor de búsqueda llega a su página, solo ve una etiqueta div con el id=root.
+// si el motor de búsqueda accede a una página pre-renderizada, todo el contenido estará presente en el código fuente, lo que ayudará a indexar la página, lo que lleva a mejores clasificaciones de busqueda
+
+// Hay dos formas de pre-renderización: 1) Static Generation  2) Server-side Rendering
+
+////////////////////              Static Generation             //////////////////////
+//un método de pre-renderizado en el que las páginas HTML se generan en el momento de compilación.
+// El HTML con todos los datos que componen el contenido de la página web se generan anticipadamente cuando se construye la aplicación
+// Ejemplos de cuando usar Static Generation: Blog pages, e-commerce product pages, documentation and marketing pages
+
+//  Next Js, por defecto, sin ninguna configuración, genera estáticamente cada página de nuestra aplicación cuando la construimos para producción(build de la app). Esto permite que la página sea almacenada en caché por un CDN e indexada por un motor de búsqueda.
+
+////////////////////      Static Generation with getStaticProps        //////////////////////
+
+// Primero obtenemos algunos datos externos y despues genera el HTML
+//En next js cuando exportamos un page component, tambien podemos exportar una funcion asincrona llamada getStaticProps(). Si exportamos esa función, se ejecutará en el build time en producción y dentro de la función se puede obtener datos externos (fetch) y enviarlos como props al componente
+// Ejemplo:
+
+// function UserList({users}){
+//     return <h1>Lista of users</h1>
+// }
+
+// export default UserList
+
+// export async function getStaticProps(){
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data) // este console.log se ve en la terminal y no en la consola del navegador
+//     // Hay que retornar siempre un objeto, con una prop llamada props que tambien es un objeto
+//     return {
+//         props:{
+//             users: data, // data es la lista de usuarios que obtuvimos en el fetch
+//         }
+//     }
+
+//     // Esto se le pasa como prop a la funcion del componente de pagina (UserList({prop})) se puede destructurar tmb y poner({users})
+//     // Despues este users lo recorremos con un map y vamos mostrando cada uno como siempre
+// }
+
 // ESTILOS CSS
 // Si queremos darle estilos a elementos generales para toda la aplicación, (por ejemplo darle estilos a una etiqueta h1) debemos hacerlo en global.css
 
